bab62d9a185ca6ffe099e38d37c771f66f2cc3e6
Gosuke Miyashita <gosukenator@gmail.com>
2007-02-21 01:19:36 +0000

    Test::IMAP, Test::SSH: Added. Thanks to franck!

---
+++ lib/Assurer/Plugin/Test/IMAP.pm
@@ -0,0 +1,53 @@
+package Assurer::Plugin::Test::IMAP;
+
+use strict;
+use warnings;
+use base qw( Assurer::Plugin::Test );
+use Net::IMAP::Simple;
+use Assurer::Test;
+
+use Data::Dumper;
+
+sub register {
+    my $self = shift;
+		$self->register_tests( qw/ connect / );
+}
+
+sub connect {
+	my ($self, $context, $args) = @_;
+
+  my $conf = $self->conf;
+
+  my $host    = $conf->{host}     || $context->conf->{host};
+	my $timeout = $conf->{timeout}  || 90;
+	my $port		= $conf->{port}			|| 143;
+
+	my $imap = Net::IMAP::Simple->new($host, [port => $port, timeout => $timeout]);
+	
+	ok($imap, "smtp ok $host");
+}
+
+1;
+__END__
+
+=head1 NAME
+
+Assurer::Plugin::Test::IMAP - Test for IMAP
+
+=head1 SYNOPSIS
+
+  - module: IMAP
+		name: IMAP Test
+    config:
+      timeout: 5
+			port: 143
+    role: imap
+	
+=head1 DESCRIPTION
+
+test server by Net::IMAP::Simple.
+
+=head1 AUTHOR
+
+Franck Cuny <franck.cuny __AT__ gmail.com>
+
---
+++ lib/Assurer/Plugin/Test/SSH.pm
@@ -0,0 +1,58 @@
+package Assurer::Plugin::Test::SSH;
+
+use strict;
+use warnings;
+use base qw( Assurer::Plugin::Test );
+use Net::Scan::SSH::Server::Version;
+use Assurer::Test;
+
+use Data::Dumper;
+
+sub register {
+    my $self = shift;
+		$self->register_tests( qw/ connect / );
+}
+
+sub connect {
+	my ($self, $context, $args) = @_;
+
+  my $conf = $self->conf;
+
+  my $host    = $conf->{host}     || $context->conf->{host};
+	my $timeout = $conf->{timeout}  || 8;
+	my $port		= $conf->{port}			|| 22;
+
+	my $ssh = Net::Scan::SSH::Server::Version->new({
+	    host    => $host,
+	    timeout => $timeout,
+			port    => $port,
+	});
+	
+	my $results = $ssh->scan;
+	like( $results, qr/SSH-.*/, "SSH Connection is up" );
+}
+
+1;
+__END__
+
+=head1 NAME
+
+Assurer::Plugin::Test::SSH - Test for SSH
+
+=head1 SYNOPSIS
+
+  - module: SSH
+		name: SSH Test
+    config:
+      timeout: 5
+			port: 22
+    role: ssh
+	
+=head1 DESCRIPTION
+
+test server by Net::Scan::SSH::Server::Version.
+
+=head1 AUTHOR
+
+Franck Cuny <franck.cuny __AT__ gmail.com>
+
