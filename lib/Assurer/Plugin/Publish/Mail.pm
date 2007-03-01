d1d7b87596aff23b69fda82f28b523ad0574bb0b
Gosuke Miyashita <gosukenator@gmail.com>
2007-03-01 14:43:29 +0000

    Publish::Mail: Initial release.

---
+++ lib/Assurer/Plugin/Publish/Mail.pm
@@ -0,0 +1,50 @@
+package Assurer::Plugin::Publish::Mail;
+
+use strict;
+use warnings;
+use base qw( Assurer::Plugin::Publish );
+use MIME::Lite;
+
+sub init {
+    my $self = shift;
+}
+
+sub run {
+    my ( $self, $context, $args ) = @_;
+
+    my $conf = $self->conf;
+    my $msg = MIME::Lite->new(
+        From    => $conf->{from},
+        To      => $conf->{to},
+        Subject => $conf->{subject},
+        Type    => $args->{format}->type,
+        Data    => $args->{format}->content,
+    );
+
+    $msg->send;
+    $context->log(info => "Sending $conf->{subject} to $conf->{to}");
+}
+
+1;
+__END__
+
+=head1 NAME
+
+Assurer::Plugin::Publish::Mail - Send result by mail
+
+=head1 SYNOPSIS
+
+  - module: Mail
+    config:
+      subject: Test results from Assurer
+      to: miya@mizzy.org
+      from: root@mizzy.org
+
+=head1 DESCRIPTION
+
+Send reslts by mail.
+
+=head1 AUTHOR
+
+Franck Cuny
+Gosuke Miyashita
