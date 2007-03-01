99c9b070aa03ae6be325d36a7f18c3b0e9289fbb
Gosuke Miyashita <gosukenator@gmail.com>
2007-03-01 13:50:51 +0000

    Add config validation by Kwalify.Thanks to franck!

--- lib/Assurer/ConfigLoader.pm
+++ lib/Assurer/ConfigLoader.pm
@@ -4,6 +4,7 @@ use strict;
 use warnings;
 use Carp;
 
+use Kwalify qw(validate);
 use YAML;
 
 sub new {
@@ -14,14 +15,24 @@ sub new {
 sub load {
     my ( $self, $stuff, $context ) = @_;
 
+    my $assets_dir = File::Spec->catfile($FindBin::Bin, 'assets');
+    my $schema_file = File::Spec->catfile( $assets_dir, 'kwalify', 'schema.yaml' );
+
     my $config;
-    if ( ( !ref($stuff) && $stuff eq '-' ) ||
-        ( -e $stuff && -r _) ) {
-        $config = YAML::LoadFile($stuff);
+    if (   ( !ref($stuff) && $stuff eq '-' )
+        || ( -e $stuff && -r _ ) )
+    {
+        my $res = validate( YAML::LoadFile($schema_file),
+            YAML::LoadFile($stuff) );
+        $context->log( error => $res ) unless $res == 1;
         $context->{config_path} = $stuff if $context;
+        $config = YAML::LoadFile($stuff);
     }
     elsif ( ref($stuff) && ref($stuff) eq 'SCALAR' ) {
-        $config = YAML::Load(${$stuff});
+        my $res
+            = validate( YAML::LoadFile($schema_file), YAML::Load($stuff) );
+        $context->log( error => $res ) unless $res == 1;
+        $config = YAML::Load( ${$stuff} );
     }
     elsif ( ref($stuff) && ref($stuff) eq 'HASH' ) {
         $config = Storable::dclone($stuff);
