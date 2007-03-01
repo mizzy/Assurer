259a6db8545bd4d01d7439c8e2e0c06099df6d6a
Gosuke Miyashita <gosukenator@gmail.com>
2007-03-01 15:28:49 +0000

    Assurer::ConfigLoader: a little refactoring.

--- lib/Assurer/ConfigLoader.pm
+++ lib/Assurer/ConfigLoader.pm
@@ -17,19 +17,20 @@ sub load {
 
     my $assets_dir = File::Spec->catfile($FindBin::Bin, 'assets');
     my $schema_file = File::Spec->catfile( $assets_dir, 'kwalify', 'schema.yaml' );
+    my $schema = YAML::LoadFile( $schema_file );
 
     my $config;
     if (   ( !ref($stuff) && $stuff eq '-' )
         || ( -e $stuff && -r _ ) )
     {
         $config = YAML::LoadFile($stuff);
-        my $res = validate( YAML::LoadFile($schema_file), $config );
+        my $res = validate( $schema, $config );
         $context->log( error => $res ) unless $res == 1;
         $context->{config_path} = $stuff if $context;
     }
     elsif ( ref($stuff) && ref($stuff) eq 'SCALAR' ) {
         $config = YAML::Load( ${$stuff} );
-        my $res = validate( YAML::LoadFile($schema_file), $config );
+        my $res = validate( $schema, $config );
         $context->log( error => $res ) unless $res == 1;
     }
     elsif ( ref($stuff) && ref($stuff) eq 'HASH' ) {
