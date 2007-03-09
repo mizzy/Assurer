a7c39a0ab288e78296e6af2c4cb8a74038dd3c65
Gosuke Miyashita <gosukenator@gmail.com>
2007-03-09 16:07:08 +0000

    Assurer::Test::WWW::Mechanize: Fix for no plan error.

--- lib/Assurer/Test/WWW/Mechanize.pm
+++ lib/Assurer/Test/WWW/Mechanize.pm
@@ -3,6 +3,7 @@ package Assurer::Test::WWW::Mechanize;
 use strict;
 use warnings;
 use Test::WWW::Mechanize;
+use Test::Builder;
 
 sub new {
     my $class = shift;
@@ -10,6 +11,9 @@ sub new {
     my $self = {
         mech => Test::WWW::Mechanize->new,
     };
+
+    my $test = Test::Builder->new;
+    $test->no_plan;
 
     bless $self, $class;
 }
