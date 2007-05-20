5169f727d26018633ba464fb19f75102412535f6
Gosuke Miyashita <gosukenator@gmail.com>
2007-05-20 12:20:19 +0000

    Test::HTTP: Experimental integration with Web::Scraper::Config.

--- lib/Assurer/Plugin/Test/HTTP.pm
+++ lib/Assurer/Plugin/Test/HTTP.pm
@@ -9,7 +9,7 @@ use Assurer::Test;
 
 sub register {
     my $self = shift;
-    $self->register_tests( qw/ status content server / );
+    $self->register_tests( qw/ status content server scrape / );
 }
 
 sub status {
@@ -26,30 +26,30 @@ sub status {
     my $scheme = $conf->{ scheme } || $port eq '80' ? 'http' : 'https';
 
     # check if the host have allready the scheme and port
-    if ($host !~ /$scheme/){
+    if ($host and $host !~ /$scheme/){
         $host = "$scheme://$host";
     }
 
-    if ($host !~ /$port/){
+    if ($host and $host !~ /$port/){
         $host = "$host:$port";
     }
 
-    if ($host !~ /$path/){
+    if ($host and $host !~ /$path/){
         $host = "$host$path";
     }
 
     $path = "/$path" if $path !~ m!^/!;
-    $self->{ url } = $conf->{ uri } || $host;
-    my $code = $conf->{ code } || 200;
+    $self->{uri} = $conf->{uri} || $host;
+    my $code = $conf->{code} || 200;
 
     my $ua = LWP::UserAgent->new;
-    $ua->agent( $agent );
+    $ua->agent($agent);
 
-    my $req = HTTP::Request->new( GET => $self->{ url } );
-    $self->{ res } = $ua->request( $req );
+    my $req = HTTP::Request->new( GET => $self->{uri} );
+    $self->{res} = $ua->request($req);
 
-    is( $self->{ res }->code,
-        $code, "HTTP status code of $self->{url} is 200" );
+    is( $self->{res}->code,
+        $code, "HTTP status code of $self->{uri} is 200" );
 }
 
 sub content {
@@ -58,7 +58,7 @@ sub content {
     my $content = $self->conf->{ content };
     if ( $content ) {
         like( $self->{ res }->content,
-              qr/$content/, "Content of $self->{url} matches '$content'",
+              qr/$content/, "Content of $self->{uri} matches '$content'",
         );
     }
 }
@@ -66,11 +66,70 @@ sub content {
 sub server {
     my ( $self, $context, $args ) = @_;
 
-    my $server = $self->conf->{ server };
+    my $server = $self->conf->{server};
     if ( $server ) {
-        is( $self->{ res }->server,
-            $server, "HTTPD version is " . $self->{ res }->server );
+        is( $self->{res}->server,
+            $server, "HTTPD version is " . $self->{res}->server );
     }
 }
 
+sub scrape {
+    my ( $self, $context, $args ) = @_;
+    my $conf = $self->conf;
+
+    return unless $conf->{scraper};
+
+    require Web::Scraper::Config;
+    my $scraper = Web::Scraper::Config->new({ scraper => $conf->{scraper} });
+    my $res = $scraper->scrape( URI->new($conf->{uri}) );
+
+    my $match = 0;
+    my $v = Data::Visitor::Callback->new(
+        value => sub {
+            my ( $self, $data ) = @_;
+            $match = 1 if $data =~ /$conf->{match}/i;
+        }
+    );
+    $v->visit($res);
+
+    ok($match, "Scraping result contains $conf->{match}");
+}
+
 1;
+
+=head1 NAME
+
+Assurer::Plugin::Test::HTTP - Test for HTTP
+
+=head1 SYNOPSIS
+
+  - module: HTTP
+    name: Web::Scraper test
+    config:
+      content: It works!
+    role: web
+
+  - module: HTTP
+    name: Web::Scraper test
+    config:
+      uri: http://search.ebay.com/apple-ipod-nano_W0QQssPageNameZWLRS
+      scraper:
+        - process:
+            - table.ebItemlist tr.single
+            - auctions[]
+            - scraper:
+                - process:
+                    - h3.ens>a
+                    - description
+                    - TEXT
+      match: white
+
+=head1 DESCRIPTION
+
+HTTP test plugin.
+
+=head1 AUTHOR
+
+Gosuke Miyashita <gosukenator __AT__ gmail.com>
+
+
